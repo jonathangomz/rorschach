ecc36d319bdd21e0f97af2d98d9cc22600e3e871
jonathangomz <jonicgp97@gmail.com>
2024-04-16 13:38:36 -0600

    Fixes numeric input not working manually

--- src/App.tsx
+++ src/App.tsx
@@ -60,31 +60,47 @@ function App() {
     setAddingNewFrecuency(false);
   }
 
-  function updateLocalizationValue(variable: Localization, newValue: number) {
-    if(newValue < 0) return;
+  function updateLocalizationValue(variable: Localization, newValue: string) {
+    const value = Number.parseInt(newValue);
+
+    if(isNaN(value)) return;
+    if(value < 0) return;
+
     const newMap = new Map(localization);
-    newMap.set(variable, newValue);
+    newMap.set(variable, value);
     setLocalization(newMap);    
   }
 
-  function updateDeterminantValue(variable: Determinant, newValue: number) {
-    if(newValue < 0) return;
+  function updateDeterminantValue(variable: Determinant, newValue: string) {
+    const value = Number.parseInt(newValue);
+
+    if(isNaN(value)) return;
+    if(value < 0) return;
+
     const newMap = new Map(determinant);
-    newMap.set(variable, newValue);
+    newMap.set(variable, value);
     setDeterminant(newMap);    
   }
 
-  function updateContentValue(variable: Content, newValue: number) {
-    if(newValue < 0) return;
+  function updateContentValue(variable: Content, newValue: string) {
+    const value = Number.parseInt(newValue);
+
+    if(isNaN(value)) return;
+    if(value < 0) return;
+
     const newMap = new Map(content);
-    newMap.set(variable, newValue);
+    newMap.set(variable, value);
     setContent(newMap);    
   }
 
-  function updateFrecuencyValue(variable: Frecuency, newValue: number) {
-    if(newValue < 0) return;
+  function updateFrecuencyValue(variable: Frecuency, newValue: string) {
+    const value = Number.parseInt(newValue);
+
+    if(isNaN(value)) return;
+    if(value < 0) return;
+
     const newMap = new Map(frecuency);
-    newMap.set(variable, newValue);
+    newMap.set(variable, value);
     setFrecuency(newMap);    
   }
 
@@ -162,7 +178,7 @@ function App() {
 
             {addingNewLocalization ? <div id='add_localization_div'>
               <label htmlFor='new_localization' className="block mb-2 text-sm font-medium text-gray-900 text-white">Variable</label>
-              <select id='1' onChange={(e) => addNewLocalization(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+              <select id='new_localization' onChange={(e) => addNewLocalization(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 <option disabled selected>Selecciona una Localización</option>
                 {Object.values(Localization).filter((v): v is number => typeof v === 'number').map((variable) => {
                   if(!localization.has(variable)) {
@@ -190,13 +206,13 @@ function App() {
                   <div>
                     <label htmlFor={Localization[entry[0]] + '_value'} className="block mb-2 text-sm font-medium text-gray-900 text-white">Value</label>
                     <div className="relative flex items-center max-w-[8rem]">
-                      <button onClick={() => updateLocalizationValue(entry[0], entry[1] - 1)} type="button" id="decrement-button" data-input-counter-decrement={Localization[entry[0]] + '_value'} className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                      <button onClick={() => updateLocalizationValue(entry[0], (entry[1] - 1).toString())} type="button" id="decrement-button" data-input-counter-decrement={Localization[entry[0]] + '_value'} className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                         <svg className="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
                           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h16"/>
                         </svg>
                       </button>
-                      <input type="text" id={Localization[entry[0]] + '_value'} value={entry[1]} data-input-counter={true} aria-describedby="helper-text-explanation" className="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="999" required />
-                      <button onClick={() => updateLocalizationValue(entry[0], entry[1] + 1)} type="button" id="increment-button" data-input-counter-increment={Localization[entry[0]] + '_value'} className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                      <input onChange={(e) => updateLocalizationValue(entry[0], e.target.value)} type="text" id={Localization[entry[0]] + '_value'} value={entry[1]} data-input-counter={true} aria-describedby="helper-text-explanation" className="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="999" required />
+                      <button onClick={() => updateLocalizationValue(entry[0], (entry[1] + 1).toString())} type="button" id="increment-button" data-input-counter-increment={Localization[entry[0]] + '_value'} className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                         <svg className="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16"/>
                         </svg>
@@ -245,13 +261,13 @@ function App() {
                   <div>
                     <label htmlFor={Determinant[entry[0]] + '_value'} className="block mb-2 text-sm font-medium text-gray-900 text-white">Value</label>
                     <div className="relative flex items-center max-w-[8rem]">
-                      <button onClick={() => updateDeterminantValue(entry[0], entry[1] - 1)} type="button" id="decrement-button" data-input-counter-decrement={Determinant[entry[0]] + '_value'} className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                      <button onClick={() => updateDeterminantValue(entry[0], (entry[1] - 1).toString())} type="button" id="decrement-button" data-input-counter-decrement={Determinant[entry[0]] + '_value'} className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                         <svg className="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
                           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h16"/>
                         </svg>
                       </button>
-                      <input type="text" id={Determinant[entry[0]] + '_value'} value={entry[1]} data-input-counter={true} aria-describedby="helper-text-explanation" className="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="999" required />
-                      <button onClick={() => updateDeterminantValue(entry[0], entry[1] + 1)} type="button" id="increment-button" data-input-counter-increment={Determinant[entry[0]] + '_value'} className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                      <input onChange={(e) => updateDeterminantValue(entry[0], e.target.value)} type="text" id={Determinant[entry[0]] + '_value'} value={entry[1]} data-input-counter={true} aria-describedby="helper-text-explanation" className="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="999" required />
+                      <button onClick={() => updateDeterminantValue(entry[0], (entry[1] + 1).toString())} type="button" id="increment-button" data-input-counter-increment={Determinant[entry[0]] + '_value'} className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                         <svg className="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16"/>
                         </svg>
@@ -299,13 +315,13 @@ function App() {
                   <div>
                     <label htmlFor={Content[entry[0]] + '_value'} className="block mb-2 text-sm font-medium text-gray-900 text-white">Value</label>
                     <div className="relative flex items-center max-w-[8rem]">
-                      <button onClick={() => updateContentValue(entry[0], entry[1] - 1)} type="button" id="decrement-button" data-input-counter-decrement={Content[entry[0]] + '_value'} className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                      <button onClick={() => updateContentValue(entry[0], (entry[1] - 1).toString())} type="button" id="decrement-button" data-input-counter-decrement={Content[entry[0]] + '_value'} className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                         <svg className="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
                           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h16"/>
                         </svg>
                       </button>
-                      <input type="text" id={Content[entry[0]] + '_value'} value={entry[1]} data-input-counter={true} aria-describedby="helper-text-explanation" className="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="999" required />
-                      <button onClick={() => updateContentValue(entry[0], entry[1] + 1)} type="button" id="increment-button" data-input-counter-increment={Content[entry[0]] + '_value'} className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                      <input onChange={(e) => updateContentValue(entry[0], e.target.value)} type="text" id={Content[entry[0]] + '_value'} value={entry[1]} data-input-counter={true} aria-describedby="helper-text-explanation" className="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="999" required />
+                      <button onClick={() => updateContentValue(entry[0], (entry[1] + 1).toString())} type="button" id="increment-button" data-input-counter-increment={Content[entry[0]] + '_value'} className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                         <svg className="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16"/>
                         </svg>
@@ -353,13 +369,13 @@ function App() {
                   <div>
                     <label htmlFor={Frecuency[entry[0]] + '_value'} className="block mb-2 text-sm font-medium text-gray-900 text-white">Value</label>
                     <div className="relative flex items-center max-w-[8rem]">
-                      <button onClick={() => updateFrecuencyValue(entry[0], entry[1] - 1)} type="button" id="decrement-button" data-input-counter-decrement={Frecuency[entry[0]] + '_value'} className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                      <button onClick={() => updateFrecuencyValue(entry[0], (entry[1] - 1).toString())} type="button" id="decrement-button" data-input-counter-decrement={Frecuency[entry[0]] + '_value'} className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                         <svg className="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
                           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h16"/>
                         </svg>
                       </button>
-                      <input type="text" id={Frecuency[entry[0]] + '_value'} value={entry[1]} data-input-counter={true} aria-describedby="helper-text-explanation" className="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="999" required />
-                      <button onClick={() => updateFrecuencyValue(entry[0], entry[1] + 1)} type="button" id="increment-button" data-input-counter-increment={Frecuency[entry[0]] + '_value'} className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                      <input onChange={(e) => updateFrecuencyValue(entry[0], e.target.value)} type="text" id={Frecuency[entry[0]] + '_value'} value={entry[1]} data-input-counter={true} aria-describedby="helper-text-explanation" className="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="999" required />
+                      <button onClick={() => updateFrecuencyValue(entry[0], (entry[1] + 1).toString())} type="button" id="increment-button" data-input-counter-increment={Frecuency[entry[0]] + '_value'} className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                         <svg className="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16"/>
                         </svg>
